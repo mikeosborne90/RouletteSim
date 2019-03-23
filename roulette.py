3441a8db4a3555bdcc176df56dd502b8ffe983b8
mikeosborne90 <mikeosborne1990@gmail.com>
2019-03-23 11:47:08 -0700

    - Added a few comments

--- roulette.py
+++ roulette.py
@@ -1,7 +1,10 @@
 import gameLogic
 
+#Example betting $100
 myGame = gameLogic.gameLogic(100)
+#Example betting on row 1
 myGame.chooseRowBet(1)
+#Example betting on column 1
 myGame.chooseColumnBet(1)
 myGame.showNumbersChosen()
 myGame.didIWin()
